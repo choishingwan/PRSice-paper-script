2a716638f285b42b96c5688d8ca88d86571083a2
Shing Wan Choi <choishingwan@gmail.com>
2019-02-14 16:24:41 +0000

    File to generate the summary stat and genotype files
    We need to generate genotype files because LDpred doesn't support on the fly filtering of sample and SNPs

---
+++ gen_plink.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -q HighMemLongterm.q,LowMemLongterm.q,LowMemShortterm.q
+#$ -t 1:20
+
+module add bioinformatics/plink2/1.90b3.38
+module add bioinformatics/R/3.3.3
+module add compilers/gcc/6.2.0
+
+# qsub -v file=sim5,bfile=/mnt/lustre/groups/ukbiobank/usr/sam/project/PRS/sample_overlap/raw/ukb_binary_v2,snp=/mnt/lustre/groups/ukbiobank/usr/sam/project/PRS/sample_overlap/raw/ukb_binary_v2.qc.snp,script=/mnt/lustre/groups/ukbiobank/usr/sam/project/PRS/sample_overlap/scripts/p2t.R ../scripts/run_gwas.sh
+# Need an input file prefix
+# Required input
+# bfile -> Genotype file prefix
+# snp -> SNP filtering file
+# file -> PRefix of GWAS guide
+# script -> processing script (p2t.Rfor converting binary assoc file
+filename=${file}.plink.${SGE_TASK_ID}
+{
+    read -r line
+    while read -r line; do
+        info=( ${line} )
+        out=${info[0]}
+        plink \
+            --bfile ${bfile} \
+            --extract ${snp} \
+            --pheno ${fam} \
+			--keep ${out} \
+            --make-bed \
+            --out ${out}
+        # Now add the A1 A2 column onto the association file
+        # Also compress it to save 5 times the memory space
+    done 
+} < "$filename"
